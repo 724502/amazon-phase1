751e87826fcf31897c3cb3412b24e9500f221310
eugene <eugenemuasya395@gmail.com>
2023-06-24 13:07:09 +0300

    DomContentLoaded feature added

--- Script/001-amazon.js
+++ Script/001-amazon.js
@@ -1,51 +1,77 @@
+document.addEventListener("DOMContentLoaded", () =>{
 
+  let productsHTML="";
 
-let productsHTML="";
+  products.forEach((product)=> {
 
-products.forEach((product)=> {
-
-  productsHTML+=`
-    <div class="product-container">
-      <img src="${product.image}"; style="width:300px">
-      <div style="font-family: Verdana, ;" class="product-description">
-        ${product.name}
-      </div>
-     <div> 
-        <img class="star-rating" src="/amazonicons/rating-${product.rating *10 }.png">            
-     </div>
-
-     <div class="product-price">${product.priceCents /100}</div>  
-
+    productsHTML+=`
+      <div class="product-container">
+        <img src="${product.image}"; style="width:300px">
+        <div style="font-family: Verdana, ;" class="product-description">
+          ${product.name}
+        </div>
       <div> 
-        <select class="cart-quantity" >
-          <option selected="" value="0">0</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
+          <img class="star-rating" src="/amazonicons/rating-${product.rating *10 }.png"> 
+
+      <div class="product-price">${product.priceCents /100}</div>  
+
+        <div> 
+          <select class="cart-quantity" >
+            <option selected="" value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+        <button class="addtocart-css js-addto-cart"data-product-name ="${product.name}">
+        Add to cart</button>
       </div>
-      <button class="addtocart-css js-addto-cart data-product-name ="${product.name}">
-      Add to cart</button>
-    </div>
-  `;
-}); 
+    `;
+  }); 
  
-document.querySelector(".js-product-grid").innerHTML = productsHTML; 
+  document.querySelector(".js-product-grid").innerHTML = productsHTML; 
 
 
-document.querySelectorAll(".js-addto-cart").forEach((button)=> {
-  button.addEventListener("click",()=>{
-     
-    console.log(button.dataset.productName);
+  document.querySelectorAll(".js-addto-cart").forEach((button)=> {
+      button.addEventListener("click",()=>{
+        
+      const productName=button.dataset.productName;
+        
+        let matchingitem;
+      cart.forEach((item) =>{
+        if (productName===item.productName) {
+            matchingitem=item;
+        }
+      });
+
+      if (matchingitem) {
+        matchingitem.quantity +=1
+      }else{
+        cart.push({
+          productName:productName,
+          quantity: 1
+        });
+      }
+      let cartQuantity =0;
+      cart.forEach((item)=>{
+        cartQuantity +=item.quantity;
+      });
+
+
+      document.querySelector(".js-cart-quantity").innerHTML =cartQuantity; 
+    });
   });
+
 });
+
+
  
 
 
--- Script/001-amazonData.js
+++ Script/001-amazonData.js
@@ -1,56 +1,55 @@
-const products =[{
-  image:"/amazon images/laptop.jpg",
-  name:"hp 6th generation 850g",
-  rating: 4.5,
-  priceCents: 1032          
- },{
-   image:"/amazon images/basketballs.jpg",
-   name:"high quality basketball",
-   rating:4.0,
-   priceCents:4032
- },{
-   image:"/amazon images/canvas shoes.jpg",
-   name:"Red and white canvas shoes",
-   rating:4.5,
-   priceCents:620
- },{
-   image:"/amazon images/food.jpg",
-   name:"baked meat balls",
-   rating:5.0,
-   priceCents:5445
- },{
-  image:"/amazon images/streacher.jpg",
-  name:"flexible t-shirts itallian made",
-  rating:4.5,
-  priceCents:5432
- },{
-  image:"/amazon images/tshirt.jpg",
-  name:"t-shirt short-sleved",
-  rating:4.0,
-  priceCents:3200
- },{
-  image:"/amazon images/hood.jpg",
-  name:"coloured extra-large hoodie",
-  rating:5.0,
-  priceCents:6599
- },{
-  image:"/amazon images/socks.jpg",
-  name:"cotton uni-coulored socks",
-  rating:4.5,
-  priceCents:3021
- },{
-  image:"/amazon images/graduation matter.jpg",
-  name:"black graduation ware",
-  rating:5.0,
-  priceCents:7534
- },{
-  image:"/amazon images/gift card.jpg",
-  name:"Amazon gift card",
-  rating:4.5,
-  priceCents:2000
- },{
-  image:"/amazon images/gift card.jpg",
-  name:"Amazon gift card",
-  rating:4.5,
-  priceCents:2000
- }];
+document.addEventListener("DOMContentLoaded", () =>{
+
+  const products =[{
+      image:"/amazon images/laptop.jpg",
+      name:"hp 6th generation 850g",
+      rating: 4.5,
+      priceCents: 1032          
+    },{
+      image:"/amazon images/basketballs.jpg",
+      name:"high quality basketball",
+      rating:4.0,
+      priceCents:4032
+    },{
+      image:"/amazon images/canvas shoes.jpg",
+      name:"Red and white canvas shoes",
+      rating:4.5,
+      priceCents:620
+    },{
+      image:"/amazon images/food.jpg",
+      name:"baked meat balls",
+      rating:5.0,
+      priceCents:5445
+    },{
+      image:"/amazon images/streacher.jpg",
+      name:"flexible t-shirts itallian made",
+      rating:4.5,
+      priceCents:5432
+    },{
+      image:"/amazon images/tshirt.jpg",
+      name:"t-shirt short-sleved",
+      rating:4.0,
+      priceCents:3200
+    },{
+      image:"/amazon images/hood.jpg",
+      name:"coloured extra-large hoodie",
+      rating:5.0,
+      priceCents:6599
+    },{
+      image:"/amazon images/socks.jpg",
+      name:"cotton uni-coulored socks",
+      rating:4.5,
+      priceCents:3021
+    },{
+      image:"/amazon images/graduation matter.jpg",
+      name:"black graduation ware",
+      rating:5.0,
+      priceCents:7534
+    },{
+      image:"/amazon images/gift card.jpg",
+      name:"Amazon gift card",
+      rating:4.5,
+      priceCents:2000
+  }];
+     
+});
